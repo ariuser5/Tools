3b78e71c3d7bf69b36880b62c2a35c6736fc3dcd
Darius_EC <dariusciuverca@gmail.com>
2019-06-08 00:52:01 +0300

    Defined Position2 and Vector2 structs.

---
+++ Utility2D/ICoordinate2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility2D
+{
+    interface ICoordinate2
+    {
+
+        double X { get; }
+        double Y { get; }
+
+    }
+}
---
+++ Utility2D/Vector2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility2D
+{
+    public struct Vector2 : ICoordinate2
+    {
+
+        public static byte decimalPrecision = 8;
+        public static Vector2 origin = new Vector2(0d, 0d);
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)] double _x;
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)] double _y;
+
+        public double X {
+            get {
+                return _x;
+            }
+            set {
+                _x = Math.Round(value, decimalPrecision);
+            }
+        }
+
+        public double Y {
+            get {
+                return _y;
+            }
+            set {
+                _y = Math.Round(value, decimalPrecision);
+            }
+        }
+
+        public Vector2(double xCoord, double yCoord) {
+            _x = Math.Round(xCoord, decimalPrecision);
+            _y = Math.Round(yCoord, decimalPrecision);
+        }
+    }
+}
